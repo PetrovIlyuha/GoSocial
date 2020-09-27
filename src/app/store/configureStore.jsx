fd06106fff59d4fb992f8fcf716ea7fcbdfa533d
PetrovIlyuha <dnamix1@gmail.com>
2020-09-27 11:14:53 +0300

    redux store config with thunk

---
+++ src/app/store/configureStore.jsx
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import rootReducer from "./rootReducer.js"
+import thunk from "redux-thunk"
+import { verifyAuth } from "../../features/auth/authActions.js"
+
+export function configureStore() {
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  )
+  store.dispatch(verifyAuth())
+  return store
+}

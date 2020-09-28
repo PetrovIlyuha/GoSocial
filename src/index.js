dcdcc5473b8709b5d3e2ed8edf1bfa9810a8e66a
PetrovIlyuha <dnamix1@gmail.com>
2020-09-28 16:16:10 +0300

    async reducer to control loading states in the application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "semantic-ui-css/semantic.min.css"
-import "./app/layout/styles.css"
 import "react-toastify/dist/ReactToastify.min.css"
 import "react-calendar/dist/Calendar.css"
 import App from "./app/layout/App.jsx"
@@ -10,6 +9,8 @@ import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { configureStore } from "./app/store/configureStore"
 import ScrollToTop from "./app/layout/ScrollToTop"
+
+import "./app/layout/styles.css"
 
 const rootEl = document.getElementById("root")
 

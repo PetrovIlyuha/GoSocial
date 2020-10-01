12301a9093131fc8ed68d6f6d5414e6061cb0286
PetrovIlyuha <dnamix1@gmail.com>
2020-10-01 18:26:39 +0300

    profile redux slice actions-creators

---
+++ src/features/profiles/profileActions.js
@@ -0,0 +1,53 @@
+import {
+  LISTEN_TO_CURRENT_USER_PROFILE,
+  LISTEN_TO_FOLLOWERS,
+  LISTEN_TO_FOLLOWINGS,
+  LISTEN_TO_SELECTED_PROFILE,
+  LISTEN_TO_USER_EVENTS,
+  LISTEN_TO_USER_PHOTOS,
+  SET_FOLLOW_USER,
+  SET_UNFOLLOW_USER,
+} from "./profileConstants"
+
+export const listenToCurrentUserProfile = profile => {
+  return {
+    type: LISTEN_TO_CURRENT_USER_PROFILE,
+    payload: profile,
+  }
+}
+
+export const listenToSelectedProfile = profile => {
+  return {
+    type: LISTEN_TO_SELECTED_PROFILE,
+    payload: profile,
+  }
+}
+
+export const listenToUserPhotos = photos => {
+  return {
+    type: LISTEN_TO_USER_PHOTOS,
+    payload: photos,
+  }
+}
+
+export const listenToUserEvents = events => ({
+  type: LISTEN_TO_USER_EVENTS,
+  payload: events,
+})
+
+export const listenToFollowers = followers => ({
+  type: LISTEN_TO_FOLLOWERS,
+  payload: followers,
+})
+
+export const listenToFollowings = followings => ({
+  type: LISTEN_TO_FOLLOWINGS,
+  payload: followings,
+})
+
+export const setFollowUser = () => ({
+  type: SET_FOLLOW_USER,
+})
+export const setUnfollowUser = () => ({
+  type: SET_UNFOLLOW_USER,
+})
